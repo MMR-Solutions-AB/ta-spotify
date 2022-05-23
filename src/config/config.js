6c83fb92571df17c702a2a0e60e56a8969fcc415
marcuspousette <marcuspousette@gmail.com>
2022-05-23 15:13:31 +0200

    changed made on monday w2

---
+++ src/config/config.js
@@ -0,0 +1,17 @@
+export const authEndpoint = "https://accounts.spotify.com/authorize";
+export const clientId = "d0b5aa5a208c477d83d39ee5eadb7ccd";
+export const liveURL = "https://techover-spotify.herokuapp.com/";
+export const devURL = "http://localhost:3000";
+export const redirectURL = devURL;
+export const scopes = [
+  "user-read-currently-playing",
+  "user-read-recently-played",
+  "user-read-playback-state",
+  "user-top-read",
+  "user-modify-playback-state",
+  "streaming",
+];
+
+export const accessUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectURL}&scope=${scopes.join(
+  "%20"
+)}&response_type=token&show_dialog=true`;
