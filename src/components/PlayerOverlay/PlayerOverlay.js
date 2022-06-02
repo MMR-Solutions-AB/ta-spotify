11dbc4c786455da1aa87a59e9b2f8c18cbc6c66b
marcuspousette <marcuspousette@gmail.com>
2022-06-02 09:53:57 +0200

    arrow

--- src/components/PlayerOverlay/PlayerOverlay.js
+++ src/components/PlayerOverlay/PlayerOverlay.js
@@ -1,6 +1,7 @@
-import { Box, Typography, Grid } from "@mui/material";
+import { Box, Typography, Grid, IconButton } from "@mui/material";
 import React from "react";
 import PlayerControlls from "../PlayerControlls/PlayerControlls";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 
 const PlayerOverlay = ({ sliderStyle, spotifyApi }) => {
   return (
@@ -15,11 +16,20 @@ const PlayerOverlay = ({ sliderStyle, spotifyApi }) => {
         top: 0,
         left: 0,
         transition: "all 0.3s",
-        transform: "translateY(100vh)",
+        // transform: "translateY(100vh)",
       }}
     >
       <Grid container spacing={2}>
-        <Grid item xs={12}>
+        <Grid item xs={3}>
+          <IconButton
+            onClick={() => {
+              console.log("asdasd");
+            }}
+          >
+            <KeyboardArrowDownIcon />
+          </IconButton>
+        </Grid>
+        <Grid item xs={9}>
           <Typography variant="body1" sx={{ color: "text.primary" }}>
             Cali bound
           </Typography>
