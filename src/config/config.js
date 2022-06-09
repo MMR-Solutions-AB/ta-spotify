2635fe09ce8708d44d55eb460dda3a9bbeb29db0
Marcus Pousette <marcuspousette@gmail.com>
2022-06-09 09:40:41 +0200

    added live url

--- src/config/config.js
+++ src/config/config.js
@@ -1,30 +1,30 @@
-export const authEndpoint = "https://accounts.spotify.com/authorize";
-export const clientId = "d0b5aa5a208c477d83d39ee5eadb7ccd";
-export const liveURL = "https://techover-spotify.herokuapp.com/";
-export const devURL = "http://localhost:3000/";
-export const redirectURL = devURL;
+export const authEndpoint = 'https://accounts.spotify.com/authorize';
+export const clientId = 'd0b5aa5a208c477d83d39ee5eadb7ccd';
+export const liveURL = 'https://ta-spotify.herokuapp.com/';
+export const devURL = 'http://localhost:3000/';
+export const redirectURL = liveURL;
 export const scopes = [
-  "playlist-read-collaborative",
-  "playlist-modify-public",
-  "playlist-read-private",
-  "playlist-modify-private",
-  "app-remote-control",
-  "streaming",
-  "user-read-email",
-  "user-read-private",
-  "user-library-modify",
-  "user-library-read",
-  "user-top-read",
-  "user-read-playback-position",
-  "ugc-image-upload",
-  "user-modify-playback-state",
-  "user-read-playback-state",
-  "user-read-currently-playing",
-  "user-follow-modify",
-  "user-follow-read",
-  "user-read-recently-played",
+  'playlist-read-collaborative',
+  'playlist-modify-public',
+  'playlist-read-private',
+  'playlist-modify-private',
+  'app-remote-control',
+  'streaming',
+  'user-read-email',
+  'user-read-private',
+  'user-library-modify',
+  'user-library-read',
+  'user-top-read',
+  'user-read-playback-position',
+  'ugc-image-upload',
+  'user-modify-playback-state',
+  'user-read-playback-state',
+  'user-read-currently-playing',
+  'user-follow-modify',
+  'user-follow-read',
+  'user-read-recently-played',
 ];
 
 export const accessUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectURL}&scope=${scopes.join(
-  "%20"
+  '%20'
 )}&response_type=token&show_dialog=true`;
